766e9c9c54a70414f5e50dd7689370df2ef631e7
Ashish Kumar <ashish.kumar5@lavainternational.in>
2025-07-03 11:11:13 +0530

    fixed port error

--- src/MQTTServer.cpp
+++ src/MQTTServer.cpp
@@ -7,10 +7,6 @@ FireStoreResultCallback* MQTTServer::callback = nullptr;
 WiFiClientSecure espClient;
 PubSubClient mqtt_client(espClient);
 
-#ifndef MQTT_PORT
-    #define MQTT_PORT 100
-#endif
-
 const char* emqx_cert = R"EOF(
 {{MQTT_CONNECTION_CERT}}
 )EOF";
